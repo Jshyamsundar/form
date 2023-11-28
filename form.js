4b64c5a0b79df1c5360e2db67b6d80c454799462
Jshyamsundar <143583521+Jshyamsundar@users.noreply.github.com>
2023-11-28 17:26:33 +0530

    Add files via upload

--- form.js
+++ form.js
@@ -1,65 +1,73 @@
 const form = document.getElementById("form");
-const Username = document.getElementById("Username");
-const Email = document.getElementById("Email");
-const Password = document.getElementById("Password");
-const Password1 = document.getElementById("Password1");
+const username = document.getElementById("username");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const cpassword = document.getElementById("cpassword");
 
-form.addEventListener('submit',e=>{
+form.addEventListener('submit', (e) => {
     e.preventDefault();
-    checkInputs();
-});
-function checkInputs(){
-    const UsernameValue = Username.Value.trim();
-    const EmailValue = Email.Value.trim();
-    const PasswordValue = Password.Value.trim();
-    const Password1Value = Password1.Value.trim();
-}
- if(UsernameValue===""){
-    setErrorFor(Username,"username cannot be blank");
-}
- else{
-    setSuccessFor(Username);
-}
+    validateInputs();
 
-if(EmailValue===""){
-    setErrorFor(Email,"email cannot be blank");
-}
-else if(!isemail(EmailValue)){
-    setErrorFor(Email("not a valid email"));
-}
-else{
-    setSuccessFor(Email);
-}
-
-if(PasswordValue===""){
-    setErrorFor(Password,"password cannot be blank");
-}
-else{
-    setSuccessFor(Password);
-}
-
-if(Password1Value===""){
-    setErrorFor(Password1,"password cannot be blank");
-}
-else if(PasswordValue!==Password1Value){
-    setErrorFor(Password1,"passwords does not match");
-}
-else{
-    setSuccessFor(Password1);
-}
-
-function setErrorFor(input,message){
-    const formControl = input.parentElement;
-    const small = formControl.querySelector("small");
-    formControl.className = "form-control error";
-    small.innerText = message;
-}
-function setSuccessFor(input){
-    const formControl = input.parentElement;
-    formControl.className = "form-control success";
-}
-function isemail(Email){
-    return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
-}
+})
+function validateInputs() {
+    const usernameVal = username.value.trim();
+    const emailVal = email.value.trim();
+    const passwordVal = password.value.trim();
+    const cpasswordVal = cpassword.value.trim();
 
 
+    if (usernameVal === '') {
+        setError(username, 'username is required');
+    }
+    else {
+        setSuccess(username);
+    }
+
+    if (emailVal === '') {
+        setError(email, 'email is required');
+    }
+    else if (!isEmail(emailVal)) {
+        setError(email, "please enter a valid email");
+    }
+    else {
+        setSuccess(email);
+    }
+
+    if (passwordVal === '') {
+        setError(password, "password is required");
+    }
+    else if (passwordVal.length < 8) {
+        setError(password, "password must be atleast 8 characters");
+    }
+    else {
+        setSuccess(password);
+    }
+    if (cpasswordVal === '') {
+        setError(cpassword, "confirm password is required");
+    }
+    else if (cpasswordVal !== passwordVal) {
+        setError(cpassword, "password does not match");
+
+    }
+    else {
+        setSuccess(cpassword);
+    }
+
+}
+function setError(element, message) {
+    const inputs = element.parentElement;
+    const errorElement = inputs.querySelector('.error')
+    errorElement.innerText = message;
+    inputs.classList.add('error')
+    inputs.classList.remove('success')
+}
+function setSuccess(element) {
+    const inputs = element.parentElement;
+    const errorElement = inputs.querySelector('.error')
+    errorElement.innerText = '';
+    inputs.classList.add('success')
+    inputs.classList.remove('error')
+}
+function isEmail(email){
+    return/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
+}
